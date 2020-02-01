e795cf0408acb8077d4623d355078d0f94ea9ecc
William Guo <william.guo@plexure.com>
2020-02-01 20:51:56 +1300

    Add project files.

---
+++ AutoFixture.xUnit2.AutoMoq/AutoMockDataAttribute.cs
@@ -0,0 +1,11 @@
+﻿using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+
+namespace AutoFixture.xUnit2.AutoMoq
+{
+    public class AutoMockDataAttribute : AutoDataAttribute
+    {
+        public AutoMockDataAttribute()
+            : base(() => new Fixture().Customize(new AutoMoqCustomization())) { }
+    }
+}
