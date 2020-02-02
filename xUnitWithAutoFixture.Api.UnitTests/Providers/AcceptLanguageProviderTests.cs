3221e59f18439cf780ee7c4e1106115c760a2492
William Guo <william.guo@plexure.com>
2020-02-02 23:35:58 +1300

    rename GetAcceptedLocales_ReturnsCorrectAcceptedLocaleList

--- xUnitWithAutoFixture.Api.UnitTests/Providers/AcceptLanguageProviderTests.cs
+++ xUnitWithAutoFixture.Api.UnitTests/Providers/AcceptLanguageProviderTests.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using AutoFixture.xUnit2.AutoMoq;
 using AutoFixture.Xunit2;
-using AutoFixture.xUnit2.AutoMoq;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Net.Http.Headers;
@@ -13,16 +12,17 @@ namespace xUnitWithAutoFixture.Api.UnitTests.Providers
     public class AcceptLanguageProviderTests
     {
         [Theory, AutoMockData]
-        public void GetAcceptedLocales_When_Test(
+        public void GetAcceptedLocales_ReturnsCorrectAcceptedLocaleList(
             [Frozen] IHttpContextAccessor httpContextAccessor,
             AcceptLanguageProvider sut)
         {
             // arrange
             var defaultHttpContext = new DefaultHttpContext();
-            defaultHttpContext.Request.Headers["Accept-Language"] = "en-NZ,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,en-GB;q=0.6,en-US;q=0.5";
+            defaultHttpContext.Request.Headers[HeaderNames.AcceptLanguage] = "en-NZ,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,en-GB;q=0.6,en-US;q=0.5";
             Mock.Get(httpContextAccessor)
                 .Setup(x => x.HttpContext)
                 .Returns(defaultHttpContext);
+
             // act
             var acceptedLocales = sut.GetAcceptedLocales();
 
