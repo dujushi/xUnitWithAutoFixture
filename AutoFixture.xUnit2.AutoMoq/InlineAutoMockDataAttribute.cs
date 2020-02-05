2ca2a757a2822722ace71a6d4c617385f9e8c036
William Guo <william.guo@plexure.com>
2020-02-05 16:42:30 +1300

    Add InlineAutoMockDataAttribute

---
+++ AutoFixture.xUnit2.AutoMoq/InlineAutoMockDataAttribute.cs
@@ -0,0 +1,10 @@
+﻿using AutoFixture.Xunit2;
+
+namespace AutoFixture.xUnit2.AutoMoq
+{
+    public class InlineAutoMockDataAttribute : InlineAutoDataAttribute
+    {
+        public InlineAutoMockDataAttribute(params object[] values)
+            : base(new AutoMockDataAttribute(), values) { }
+    }
+}
